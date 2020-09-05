970922d977282f509159344bec2b997e1e60e028
Rachel Bock <70186075+rachel-bock@users.noreply.github.com>
2020-09-05 15:36:37 -0600

    Add functionality behind Add button

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import "./styles.css";
+const newTodoInputEl = document.getElementById("new-todo");
+const newTodoAddButtonEl = document.getElementById("add-todo");
+const filterAllButtonEl = document.getElementById("filter-all");
+const filterNotDoneButtonEl = document.getElementById("filter-not-done");
+const filterDoneButtonEl = document.getElementById("filter-done");
+let todoList[];
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+newTodoAddButtonEl.addEventListener("click", function () {
+  if(newTodoInputEl !== ""){
+    addTodo(newTodoInputEl.value);
+  }
+});
+
+function addTodo(content){
+  todoList.push({
+    content: content,
+    id: nextId++
+  })
+}
+
+filterAllButtonEl.addEventListener("click", function () {});
+
+filterNotDoneButtonEl.addEventListener("click", function () {});
+
+filterDoneButtonEl.addEventListener("click", function () {});
