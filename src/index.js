7afcef2044ad312c0da4207258dc55d1384609d9
Rachel Bock <70186075+rachel-bock@users.noreply.github.com>
2020-09-05 20:51:17 -0600

    Restore index.js to working condition

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 const newTodoInputEl = document.getElementById("new-todo");
 const newTodoAddButtonEl = document.getElementById("add-todo");
 const filterAllButtonEl = document.getElementById("filter-all");
-const filterNotDoneButtonEl = document.getElementById("filter-not-done");
 const filterDoneButtonEl = document.getElementById("filter-done");
+const filterNotDoneButtonEl = document.getElementById("filter-not-done");
 const todoListEl = document.getElementById("todo-list");
-const storageKey = "learn-web_todo-app";
+const storageKey = "twitch-learn-web_todo-app";
+const idKey = "twitch-learn-web_todo-app_id";
 let todoList = [];
 let nextId = 0;
 let filter = "none";
 
 try {
   const savedList = localStorage.getItem(storageKey);
+
   if (savedList !== null) {
     todoList = JSON.parse(savedList);
     drawTodos();
   }
-} catch (error) {}
+
+  const savedId = localStorage.getItem(idKey);
+
+  if (savedId !== null) {
+    nextId = parseInt(savedId, 10);
+  }
+} catch (error) {
+  // ignore
+}
 
 newTodoAddButtonEl.addEventListener("click", function () {
-  if (newTodoInputEl !== "") {
+  if (newTodoInputEl.value !== "") {
     addTodo(newTodoInputEl.value);
     newTodoInputEl.value = "";
     newTodoInputEl.focus();
@@ -31,6 +41,7 @@ function addTodo(content) {
     id: nextId++,
     isComplete: false
   });
+  localStorage.setItem(idKey, nextId.toString());
   localStorage.setItem(storageKey, JSON.stringify(todoList));
   drawTodos();
 }
@@ -39,12 +50,12 @@ function drawTodos() {
   todoListEl.innerHTML = "";
 
   todoList
-    .filter(function () {
+    .filter(function (todo) {
       switch (filter) {
         case "done":
           return todo.isComplete;
         case "not-done":
-          return !todo.iscomplete;
+          return !todo.isComplete;
         default:
           return true;
       }
@@ -54,21 +65,34 @@ function drawTodos() {
       const labelEl = document.createElement("label");
       const checkboxEl = document.createElement("input");
       const spanEl = document.createElement("span");
+      const deleteButtonEl = document.createElement("button");
 
       checkboxEl.setAttribute("type", "checkbox");
+
       if (todo.isComplete) {
-        checkboxEl.setAttribute("checked", todo.isComplete);
+        checkboxEl.setAttribute("checked", true);
       }
+
       checkboxEl.addEventListener("click", function () {
-        todoList = todoList.map(function (task) {
-          if (task.id === todo.id) {
-            task.iscomplete = !task.iscomplete;
+        todoList = todoList.map(function (t) {
+          if (t.id === todo.id) {
+            t.isComplete = !t.isComplete;
           }
-          return task;
+
+          return t;
         });
         localStorage.setItem(storageKey, JSON.stringify(todoList));
         drawTodos();
       });
+
+      deleteButtonEl.addEventListener("click", function () {
+        todoList = todoList.filter(function (t) {
+          return todo.id !== t.id;
+        });
+        localStorage.setItem(storageKey, JSON.stringify(todoList));
+        drawTodos();
+      });
+      deleteButtonEl.innerText = "Delete";
 
       spanEl.innerText = todo.content;
 
@@ -76,13 +100,14 @@ function drawTodos() {
       labelEl.appendChild(spanEl);
 
       listItemEl.appendChild(labelEl);
+      listItemEl.appendChild(deleteButtonEl);
 
       todoListEl.appendChild(listItemEl);
     });
 }
 
-filterAllButtonEl.addEventListener("click", function () {
-  filter = "none";
+filterDoneButtonEl.addEventListener("click", function () {
+  filter = "done";
   drawTodos();
 });
 
@@ -91,7 +116,7 @@ filterNotDoneButtonEl.addEventListener("click", function () {
   drawTodos();
 });
 
-filterDoneButtonEl.addEventListener("click", function () {
-  filter = "done";
+filterAllButtonEl.addEventListener("click", function () {
+  filter = "none";
   drawTodos();
 });
