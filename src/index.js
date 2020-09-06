94870646ab796c909ebf08f1b37e421f03dd98b9
Rachel Bock <70186075+rachel-bock@users.noreply.github.com>
2020-09-05 19:11:12 -0600

    Add functionality to draw task list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const newTodoAddButtonEl = document.getElementById("add-todo");
 const filterAllButtonEl = document.getElementById("filter-all");
 const filterNotDoneButtonEl = document.getElementById("filter-not-done");
 const filterDoneButtonEl = document.getElementById("filter-done");
+const todoListEl = document.getElementById("todo-list");
 let todoList = [];
 let nextId = 0;
 
@@ -10,6 +11,7 @@ newTodoAddButtonEl.addEventListener("click", function () {
   if (newTodoInputEl !== "") {
     addTodo(newTodoInputEl.value);
     newTodoInputEl.value = "";
+    newTodoInputEl.focus();
   }
 });
 
@@ -18,6 +20,35 @@ function addTodo(content) {
     content: content,
     id: nextId++,
     isComplete: false
+  });
+  drawTodos();
+}
+
+function drawTodos() {
+  todoListEl.innerHTML = "";
+
+  todoList.forEach(function (todo) {
+    const listItemEl = document.createElement("li");
+    const labelEl = document.createElement("label");
+    const checkboxEl = document.createElement("input");
+    const spanEl = document.createElement("span");
+
+    checkboxEl.setAttribute("type", "checkbox");
+    if (todo.isComplete) {
+      checkboxEl.setAttribute("checked", todo.isComplete);
+    }
+    checkboxEl.addEventListener("change", function () {
+      todolist = todolist.map(function (todo) {});
+    });
+
+    spanEl.innerText = todo.content;
+
+    labelEl.appendChild(checkboxEl);
+    labelEl.appendChild(spanEl);
+
+    listItemEl.appendChild(labelEl);
+
+    todoListEl.appendChild(listItemEl);
   });
 }
 
